7f7a6909a16f04dfb664c06e6b6e44329540c2f6
elisiomualumene <elisiomualumene@gmail.com>
2022-03-04 20:43:05 +0100

    :memo:

--- src/pages/api/auth/[nextauth].ts
+++ src/pages/api/auth/[nextauth].ts
@@ -1,13 +1,12 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
-    GithubProvider({
+    Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      scope: 'read:user'
     }),
-    // ...add more providers here
   ],
 })
